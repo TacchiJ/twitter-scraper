051cf57445f7da45fc596b64513865c71653de1a
Jack <jacktacchi@gmail.com>
2020-05-15 15:43:09 +0200

    renaming and restructuring

--- twitter_scraper/__main__.py
+++ twitter_scraper/__main__.py
@@ -1,12 +1,16 @@
 import os
 import logging
 import tweepy
+import asyncio
+import requests
+
+from typing import Optional, Any, Dict
 
 logger = logging.getLogger()
 
 class TwitterAPI:
 
-    def get_twitter_api(self):
+    def __init__(self):
         # Get credentials
         CONSUMER_KEY = os.getenv('TWITTER_KEY')
         CONSUMER_SECRET = os.getenv('TWITTER_SECRET_KEY')
@@ -20,12 +24,18 @@ class TwitterAPI:
         # Create API object
         api = tweepy.API(auth, wait_on_rate_limit=True, 
                 wait_on_rate_limit_notify=True)
-
         try:
             api.verify_credentials()
         except Exception as e:
             logger.error("Error creating API", exc_info=True)
             raise e
         logger.info("API created")
-        return api
+        self.api = api
 
+    def get_api(self):
+        return self.api
+
+
+if __name__ == "__main__":
+    api = TwitterAPI()
+    print(api.get_api())
