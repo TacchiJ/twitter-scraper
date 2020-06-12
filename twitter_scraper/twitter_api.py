a5991a5ee4ccaf15872fde032fcbee9140c8961b
Jack <jacktacchi@gmail.com>
2020-06-12 07:53:21 +0200

    adding docstrings

--- twitter_scraper/twitter_api.py
+++ twitter_scraper/twitter_api.py
@@ -1,8 +1,5 @@
 import asyncio
 import logging
-import os
-import pandas as pd
-import requests
 import datetime
 import tweepy
 
@@ -14,6 +11,8 @@ logger = logging.getLogger()
 class TwitterAPI:
 
     def __init__(self, key, secret_key, access_token, access_token_secret):
+        '''Setups a TwitterAPI object with provided credentials'''
+
         # Authenticate to Twitter
         auth = tweepy.OAuthHandler(key, secret_key)
         auth.set_access_token(access_token, access_token_secret)
@@ -26,6 +25,7 @@ class TwitterAPI:
         except Exception as e:
             logger.error("Error creating API", exc_info=True)
             raise e
+        
         now = datetime.datetime.now().time()
         print(f"{now}: Twitter API created")
         self.api = api
@@ -41,32 +41,37 @@ class TwitterAPI:
 
     async def get_tweets(self, queries, n=10, lang='en'):
         tasks = []
+
         for query in queries:
             tasks.append(asyncio.ensure_future(self.search_tweets(query, n, lang)))
         tweets = await asyncio.gather(*tasks)
+
         return tweets
 
-    # Searches for most recent tweets
-    async def search_tweets(self, query, n, lang):
+    async def search_tweets(self, query, n=10, lang=''):
+        '''Searches for most recent n tweets'''
         now = datetime.datetime.now().time()
         print(f"{now}: Searching for '{query}'")
         tweets = []
+
         for tweet in self.api.search(q=query, count=n, lang=lang):
             data = [query]
+
             for field in self.tweet_contents[1:]:
                 data.append(self.get_attribute(tweet, field))
             tweets.append(data)
+
         return tweets
 
-    # Recursively retrieves attributes and child attributes of an object 
-    def get_attribute(self, obj, attr):
-        if '.' in attr:
-            attrs = attr.split('.')
-            child_attr = getattr(obj, attrs[0])
-            return self.get_attribute(child_attr, attrs[1])
+    def get_attribute(self, tweet, attribute: str):
+        '''Recursively retrieves an attribute or child attribute of a tweet based on a string'''
+        if '.' in attribute:
+            attributes = attribute.split('.')
+            child_attribute = get_attribute(tweet, attributes[0])
+            return self.get_attribute(child_attribute, attributes[1])
         try:
-            return getattr(obj, attr)
+            return get_attribute(tweet, attribute)
         except AttributeError:
-            logger.error(f"Attribute not found: {attr}")
+            logger.error(f"Attribute not found: {attribute}")
             return ''
     
